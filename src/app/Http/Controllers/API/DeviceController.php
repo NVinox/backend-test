ed52c74a0250e53be67b374a866dea437214fda7
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2025-03-26 23:09:55 +0700

    feat(Validate): валидировал метод создания устройства

--- src/app/Http/Controllers/API/DeviceController.php
+++ src/app/Http/Controllers/API/DeviceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\DeviceResource;
 use App\Http\Resources\API\DeviceResourceCollection;
 use App\Models\API\DeviceModel;
 use Illuminate\Http\Request;
@@ -23,12 +24,35 @@ class DeviceController extends Controller
 
     public function getOne(string $id)
     {
-        return $this->deviceModel->find($id);
+        $device = DeviceModel::find($id);
+        return (new DeviceResource($device))->response()->setStatusCode(200);
     }
 
     public function create(Request $request)
     {
-        return $this->deviceModel->create($request);
+        $request->validate([
+            'name' => 'required',
+            'memory_volume' => 'required',
+            'ram_volume' => 'required',
+            'battery_volume' => 'required',
+            'core_frequency' => 'required',
+            'display_width' => 'required',
+            'display_height' => 'required',
+        ]);
+
+        $device = DeviceModel::create([
+            'name' => $request->input('name'),
+            'memory_volume' => $request->input('memory_volume'),
+            'ram_volume' => $request->input('ram_volume'),
+            'battery_volume' => $request->input('battery_volume'),
+            'core_counts' => $request->input('core_counts'),
+            'core_frequency' => $request->input('core_frequency'),
+            'display_width' => $request->input('display_width'),
+            'display_height' => $request->input('display_height'),
+            'nfc' => $request->input('nfc'),
+        ]);
+
+        return (new DeviceResource($device))->response()->setStatusCode(201);
     }
 
     public function update(Request $request, string $id)
